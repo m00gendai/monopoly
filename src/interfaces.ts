723a1bd289747177d20c46bf49cfc1ec4ea6d983
Marcel Weber <mrweber@gmx.ch>
2023-12-28 13:03:34 +0100

    Added lib folder Created fields lib file containing all playing field information Created interfaces file

---
+++ src/interfaces.ts
@@ -0,0 +1,23 @@
+export interface Field{
+    name: string
+    street: string
+    id: number
+    row: number
+    width: number
+    height: number
+    orientation: string
+    boardNumber: number
+    isProperty: boolean
+    propertyColor: string
+    isTrain: boolean
+    isUtility: boolean
+    isChance: boolean
+    isKanzlei: boolean
+    isEvent: boolean
+    isGo: boolean
+    isJail: boolean
+    isParking: boolean
+    isPopo: boolean
+    isSpacer: boolean
+    icon: string
+}

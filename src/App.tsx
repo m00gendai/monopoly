25e252ab7bc508adf1d51481363ca3c428d05a56
Marcel Weber <mrweber@gmx.ch>
2023-12-28 13:51:16 +0100

    Integrated Accessories component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Board from "./components/board"
-
+import Accessories from "./components/Accessories"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +8,7 @@ function App() {
   return (
    <main>
     <Board />
+    <Accessories />
    </main>
   )
 }

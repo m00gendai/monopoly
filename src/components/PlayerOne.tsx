d5d069198d42595578b35a886293ad9752073928
Marcel Weber <mrweber@gmx.ch>
2023-12-28 14:51:58 +0100

    Created rudimentary PlayerOne component*

---
+++ src/components/PlayerOne.tsx
@@ -0,0 +1,13 @@
+import s from "../styles/Player.module.css"
+import { useState } from "react"
+
+export default function PlayerOne(){
+
+    return(
+        <div 
+            className={s.player}
+
+        >
+        </div>
+    )
+}
